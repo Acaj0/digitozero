49308c4e34a02bf3eae43dc7d1f4307659befe00
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-03-26 22:08:31 -0400

    WelcomeText

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+    deviceSizes: [300, 400, 500, 1080, 1200, 1920, 2048, 3840],
+  },};
 
 export default nextConfig;
