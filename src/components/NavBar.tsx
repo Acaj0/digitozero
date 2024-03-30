20036d74327beb8fad3eb370337e0024bd9b0b4c
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-03-30 16:16:09 -0400

    AddedNewPages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function NavBar(){   
     return <div className="bg-[#151618] h-10 lg:h-24 flex justify-center items-center gap-20 text-xl italic">
-    <a>SHOWS</a>
-    <a>SETLIST</a>
-    <Image src={"/logodigitosite.png"} width={100} height={50} alt="logo da banda"/>
-    <a>GALERIA</a>
+    <a href="/shows">SHOWS</a>
+    <a href="/setlist">SETLIST</a>
+    <a href="/"><Image src={"/logodigitosite.png"} width={100} height={50} alt="logo da banda"/></a>
+    <a href="/galeria">GALERIA</a>
     <a>CONTATO</a>
  </div>
 }
