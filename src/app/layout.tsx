e16d53dee2b1fe5e019bfda0e2ab70d778e098c9
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-09-10 15:10:50 -0400

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Banda Digito Zero - Rock Autoral e Covers com Estilo Único",
   description:
-    "Conheça a Digito Zero, uma banda de Cuiabá que mistura músicas autorais e covers icônicos com uma pegada única. Com shows eletrizantes e um repertório variado, a Digito Zero conquista fãs por onde passa. Visite nosso site para saber mais sobre nossas músicas, conferir a agenda de shows e mais. Junte-se à nossa comunidade de fãs e sinta a energia do verdadeiro rock!",
+    "Conheça a Digito Zero, uma banda de rock da cidade de Cuiabá-MT que mistura músicas autorais e covers icônicos com uma pegada única. Com shows eletrizantes e um repertório variado, a Digito Zero conquista fãs por onde passa. Visite nosso site para saber mais sobre nossas músicas, conferir a agenda de shows e mais. Junte-se à nossa comunidade de fãs e sinta a energia do verdadeiro rock!",
 };
 
 export default function RootLayout({
