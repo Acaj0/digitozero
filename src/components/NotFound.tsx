ddb805a94d4f55d694c6401984a2bb6e9908532f
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-29 18:41:23 -0400

    modified:   src/components/NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -8,7 +8,7 @@ export const NotFound = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-5xl font-bold">404</h1>
-      <p className="mt-2 text-2xl">Aparentemente, você tentou acessar um show épico da nossa banda, mas… ops! Parece que a gente se perdeu em algum lugar entre o palco e a pizza do pós-show.</p>
+      <p className="mt-2 text-2xl mx-52">Aparentemente, você tentou acessar um show épico da nossa banda, mas… ops! Parece que a gente se perdeu em algum lugar entre o palco e a pizza do pós-show.</p>
 
       <Button asChild className="mt-4">
         <Link href="/">
