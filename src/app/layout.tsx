10db80504b42c28627f5921a3fc10763b388f368
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-29 16:49:36 -0400

    modified:   src/app/layout.tsx 	deleted:    src/components/DarkModeToggle.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
-
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -9,7 +8,8 @@ const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Banda Digito Zero - Rock Autoral e Covers com Estilo Único em Cuiabá",
-  keywords: "Banda, rock, autoral, Cuiaba, Várzea Grande, Mato Grosso, covers, banda de rock em cuiaba",
+  keywords:
+    "Banda, rock, autoral, Cuiaba, Várzea Grande, Mato Grosso, covers, banda de rock em cuiaba",
   description:
     "Conheça a Digito Zero, uma banda de rock da cidade de Cuiabá-MT que mistura músicas autorais e covers icônicos com uma pegada única. Com shows eletrizantes e um repertório variado, a Digito Zero conquista fãs por onde passa. Visite nosso site para saber mais sobre nossas músicas, conferir a agenda de shows e mais. Junte-se à nossa comunidade de fãs e sinta a energia do verdadeiro rock!",
 };
