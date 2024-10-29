1fd4f06ef5ca9e5568c5cc5bb3908989ca081645
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-29 14:58:30 -0400

    Teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
