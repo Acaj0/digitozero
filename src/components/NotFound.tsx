c9a0eea3e634164aadc4654b51e50edd9a893484
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-29 18:36:04 -0400

    404

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -8,11 +8,11 @@ export const NotFound = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-5xl font-bold">404</h1>
-      <p className="mt-2 text-2xl">Page not found</p>
+      <p className="mt-2 text-2xl">Aparentemente, você tentou acessar um show épico da nossa banda, mas… ops! Parece que a gente se perdeu em algum lugar entre o palco e a pizza do pós-show.</p>
 
       <Button asChild className="mt-4">
         <Link href="/">
-          <Undo className="mr-2 h-4 w-4" /> Go to homepage
+          <Undo className="mr-2 h-4 w-4" /> Voltar Para Pagina Principal
         </Link>
       </Button>
     </div>
